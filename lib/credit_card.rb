859c93ee7fe9e568373ae953fe2b153391dd4b2d
Rui Pereira <ruijosemangas@gmail.com>
2015-07-06 18:21:03 +0100

    my very simple solution

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,2 +1,33 @@
+require 'time'
+
 class CreditCard
+  def initialize(number, date, ccv, name, zip_code)
+    @number = number
+    @date = date
+    @ccv = ccv
+    @name = name
+    @zip_code = zip_code
+  end
+
+  def valid?
+    has_name? && check_zip_code && check_card_number && valid_date?
+  end
+
+  private
+  def has_name?
+   !@name.empty? 
+  end
+
+  def check_zip_code
+    @zip_code.to_s.chars.count == 5
+  end
+
+  def check_card_number
+    @number.to_s.chars.count == 16 
+  end
+
+  def valid_date?
+    date = Time.parse(@date)
+    date > Time.now
+  end
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,1 +1,50 @@
 require_relative '../lib/credit_card'
+
+describe CreditCard do
+  describe "#valid_card" do
+    it "should be valid" do
+      credit_card = CreditCard.new(1212123445123456,
+                                   "16-01-16",
+                                   321,
+                                   "rui mangas pereira",
+                                   12321)
+      credit_card.valid?.should eql(true)
+    end
+
+    it "should be invalid: expiration date in the past" do
+      credit_card = CreditCard.new(121215123456,
+                                   "12-01-11",
+                                   321,
+                                   "rui mangas pereira",
+                                   12321)
+      credit_card.valid?.should eql(false)
+    end
+
+    it "should be valid" do
+      credit_card = CreditCard.new(1212123445123456,
+                                   "15-11-16",
+                                   321,
+                                   "pedro mangas pereira",
+                                   12321)
+      credit_card.valid?.should eql(true)
+    end
+
+    it "should be invalid: invalid digit length" do
+      credit_card = CreditCard.new(12121234451456,
+                                   "19-01-11",
+                                   321,
+                                   "rui mangas pereira",
+                                   12321)
+      credit_card.valid?.should eql(false)
+    end
+
+    it "should be invalid: name not present" do
+      credit_card = CreditCard.new(12121234451456,
+                                   "19-01-11",
+                                   321,
+                                   "",
+                                   12321)
+      credit_card.valid?.should eql(false)
+    end
+  end
+end
